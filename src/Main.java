29929ce4875d39782f95fed48eb8fac859a558ad
briz1 <my1queen8my1king@gmail.com>
2024-01-03 18:23:48 +0700

    Two tasks completed

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,42 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class Main {
+    private static Object Arrays;
+    public static void main(String[] args) {
+        task1();
+        task2();
     }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
+    }
+    public static void task1() {
+        System.out.println("Задача 1");
+        int[] arr = generateRandomArray();
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+           sum += arr[i];
+        }
+        System.out.println("Сумма трат за месяц составила " + sum + " рублей");
+    }
+    public static void task2() {
+        System.out.println("Задача 2");
+        int[] arr = generateRandomArray();
+        int maxCost = -1;
+        int minCost = 201000;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]>maxCost) {
+                maxCost = arr[i];
+            }
+            if (arr[i]<minCost) {
+                minCost = arr[i];
+            }
+        }
+        System.out.println("Минимальная сумма трата за день составила " + minCost + " рублей");
+        System.out.println("Максимальная сумма трата за день составила " + maxCost + " рублей");
+            }
 }
